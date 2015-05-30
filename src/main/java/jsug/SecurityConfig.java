870c308b11d4384a49f1cd1b319e915388e61793
Toshiaki Maki <makingx@gmail.com>
2015-05-30 22:50:20 +0900

    Change PasswordEncoder
    (cherry picked from commit c5f630c477111ea97d63097794b09e689260efe2)

--- src/main/java/jsug/SecurityConfig.java
+++ src/main/java/jsug/SecurityConfig.java
@@ -10,7 +10,7 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
@@ -47,7 +47,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         @Bean
         PasswordEncoder passwordEncoder() {
-            return NoOpPasswordEncoder.getInstance();
+            return new BCryptPasswordEncoder();
         }
 
         @Override
