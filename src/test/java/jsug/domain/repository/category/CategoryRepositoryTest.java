29e26231bcf3ea300ac95c2d19b71dbd1ed2b1b4
Toshiaki Maki <makingx@gmail.com>
2015-06-15 23:57:35 +0900

    fix encoding
    (cherry picked from commit 48551ebf04eceae61ff2cf8f3b0b7e026e6fe52b)

--- src/test/java/jsug/domain/repository/category/CategoryRepositoryTest.java
+++ src/test/java/jsug/domain/repository/category/CategoryRepositoryTest.java
@@ -7,6 +7,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.List;
@@ -21,7 +22,7 @@ import static org.junit.Assert.*;
         "/sql/drop-tables.sql",
         "/db/migration/V1__create-schema.sql",
         "/sql/insert-category.sql"
-})
+}, config = @SqlConfig(encoding = "UTF-8"))
 public class CategoryRepositoryTest {
     @Autowired
     CategoryRepository categoryRepository;
