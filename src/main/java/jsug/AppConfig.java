ddc11c42e439cac092bf50e12ae491cf01a62d65
Toshiaki Maki <makingx@gmail.com>
2016-03-15 01:43:40 +0900

    Update Spring Boot 1.3.3

--- src/main/java/jsug/AppConfig.java
+++ src/main/java/jsug/AppConfig.java
@@ -6,50 +6,41 @@ import net.sf.log4jdbc.sql.jdbcapi.DataSourceSpy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
-import org.springframework.cache.CacheManager;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.concurrent.ConcurrentMapCache;
-import org.springframework.cache.support.SimpleCacheManager;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
-import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.context.annotation.*;
 import org.springframework.web.context.WebApplicationContext;
 
 import javax.sql.DataSource;
-import java.util.Arrays;
 
 @Configuration
 @EnableCaching
 public class AppConfig {
     @Autowired
     DataSourceProperties dataSourceProperties;
+    DataSource dataSource;
 
-    @Bean
-    DataSource dataSource() {
-        DataSource dataSource = DataSourceBuilder
+    @Bean(destroyMethod = "close")
+    @ConfigurationProperties(prefix = DataSourceProperties.PREFIX)
+    DataSource realDataSource() {
+        DataSourceBuilder factory = DataSourceBuilder
                 .create(this.dataSourceProperties.getClassLoader())
                 .url(this.dataSourceProperties.getUrl())
                 .username(this.dataSourceProperties.getUsername())
-                .password(this.dataSourceProperties.getPassword())
-                .build();
-        return new DataSourceSpy(dataSource);
+                .password(this.dataSourceProperties.getPassword());
+        this.dataSource = factory.build();
+        return this.dataSource;
+    }
+
+    @Primary
+    @Bean
+    DataSource dataSource() {
+        return new DataSourceSpy(this.dataSource);
     }
 
     @Bean
     @Scope(value = WebApplicationContext.SCOPE_SESSION, proxyMode = ScopedProxyMode.TARGET_CLASS)
     Cart cart() {
         return new CachingCart();
-    }
-
-    @Bean
-    CacheManager cacheManager() {
-        SimpleCacheManager cacheManager = new SimpleCacheManager();
-        cacheManager.setCaches(Arrays.asList(
-                new ConcurrentMapCache("category"),
-                new ConcurrentMapCache("goods"),
-                new ConcurrentMapCache("orderLines"),
-                new ConcurrentMapCache("sql")));
-        return cacheManager;
     }
 }
